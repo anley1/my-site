c7d149b2f8aaa9feed3c7b5a514e49c9ee2bc6bd
Alexander Leye <alexnleye@gmail.com>
2024-01-08 00:17:55 +1100

    add reading-time

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
+import { remarkReadingTime } from './src/scripts/readingTime.ts';
 import remarkToc from 'remark-toc';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
@@ -12,7 +13,7 @@ export default defineConfig({
   site: 'https://axleydev.netlify.app',
   integrations: [mdx(), sitemap(), preact(), tailwind()],
   markdown: {
-    remarkPlugins: [remarkToc],
+    remarkPlugins: [remarkToc, remarkReadingTime],
     rehypePlugins: [rehypeAccessibleEmojis],
   },
 });
