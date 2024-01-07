7d9fb2aba084e156a03a9d13ec67da0abe44c222
Alexander Leye <alexnleye@gmail.com>
2024-01-07 23:00:27 +1100

    into config

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
+import remarkToc from 'remark-toc';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://axleydev.netlify.app',
-  integrations: [mdx(), sitemap(), preact(), tailwind()]
+  integrations: [mdx(), sitemap(), preact(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeAccessibleEmojis],
+  },
 });
