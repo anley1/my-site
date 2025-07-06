68cb02a5f3051ba96492c070e65356b1bfdbd6ba
Alexander Leye <alexnleye@gmail.com>
2025-07-06 22:13:29 +1000

    fix blog spacing

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,7 @@
 document.querySelector('.hamburger')?.addEventListener('click', () => {
-    document.querySelector('.nav-links')?.classList.toggle('expanded');
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    
+    hamburger?.classList.toggle('expanded');
+    navLinks?.classList.toggle('expanded');
 })
