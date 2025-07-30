b7374ebf10c2092ffe415a5da6000dd66c812151
Alexander Leye <alexnleye@gmail.com>
2025-07-30 12:28:44 +1000

    rehype

--- astro.config.ts
+++ astro.config.ts
@@ -6,15 +6,32 @@ import remarkToc from 'remark-toc';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeUnwrapImages from "rehype-unwrap-images";
 import tailwind from "@astrojs/tailwind";
-
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alexleye.com',
-  integrations: [mdx({
-    remarkPlugins: [[remarkToc, { heading: "contents", maxDepth: 4 }], remarkReadingTime, remarkMath],
-    rehypePlugins: [rehypeAccessibleEmojis, rehypeKatex],
-  }), sitemap(), tailwind()],
+  integrations: [
+    mdx({
+      remarkPlugins: [[remarkToc, { heading: "contents", maxDepth: 4 }], remarkReadingTime, remarkMath],
+      rehypePlugins: [
+        rehypeAccessibleEmojis, 
+        rehypeKatex, 
+        rehypeUnwrapImages,
+        [
+          rehypeExternalLinks,
+          {
+            rel: ["noreferrer", "noopener"],
+            target: "_blank",
+          },
+        ],
+      ],
+    }),
+    sitemap(), 
+    tailwind()
+  ],
 });
