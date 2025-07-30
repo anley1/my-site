e8bba62241b6b9b7db8952d9e8e4c543c834c814
Alexander Leye <alexnleye@gmail.com>
2025-07-30 11:45:15 +1000

    macbook setup

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from './src/scripts/readingTime.ts';
 import remarkToc from 'remark-toc';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
-
 import tailwind from "@astrojs/tailwind";
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alexleye.com',
   integrations: [mdx({
-    remarkPlugins: [[remarkToc, { heading: "contents", maxDepth: 4 }], remarkReadingTime],
-    rehypePlugins: [rehypeAccessibleEmojis],
+    remarkPlugins: [[remarkToc, { heading: "contents", maxDepth: 4 }], remarkReadingTime, remarkMath],
+    rehypePlugins: [rehypeAccessibleEmojis, rehypeKatex],
   }), sitemap(), tailwind()],
 });
