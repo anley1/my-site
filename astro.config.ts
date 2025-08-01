c891570e07a7eccec59fbf5019c951e84829d95b
Alexander Leye <alexnleye@gmail.com>
2025-08-01 18:34:46 +1000

    astro 5 start

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@tailwindcss/vite";
+import icon from "astro-icon";
 import { remarkReadingTime } from './src/scripts/readingTime.ts';
 import remarkToc from 'remark-toc';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeUnwrapImages from "rehype-unwrap-images";
-import tailwind from "@astrojs/tailwind";
-
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alexleye.com',
   integrations: [
+    icon(),
     mdx({
       remarkPlugins: [[remarkToc, { heading: "contents", maxDepth: 4 }], remarkReadingTime, remarkMath],
       rehypePlugins: [
@@ -31,7 +31,9 @@ export default defineConfig({
         ],
       ],
     }),
-    sitemap(), 
-    tailwind()
+    sitemap()
   ],
+  vite: {
+    plugins: [tailwind()]
+  }
 });
